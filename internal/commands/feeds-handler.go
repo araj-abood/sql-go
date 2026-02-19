ae841fdeb2487bbdbab33b596f2e3baa66dea2b1
Abdullah al-araj <alaraj.abdullah02@gmail.com>
2026-02-19 22:44:23 +0300

    add. afeeds handler

---
+++ internal/commands/feeds-handler.go
@@ -0,0 +1,30 @@
+package commands
+
+import (
+	"context"
+	"fmt"
+
+	"araj.com/ar/internal/config"
+)
+
+func FeedsHandler(s *config.State, command config.Command) error {
+	feeds, err := s.Db.GetAllFeeds(context.Background())
+
+	if err != nil {
+		return err
+	}
+
+	for _, f := range feeds {
+		fmt.Printf("name:= %s\n", f.Name)
+		fmt.Printf("url:= %s\n", f.Url)
+		userThatCreatedFeed, err := s.Db.GetUseById(context.Background(), f.UserID.UUID)
+
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("username:= %s\n", userThatCreatedFeed.Name)
+	}
+
+	return nil
+}
