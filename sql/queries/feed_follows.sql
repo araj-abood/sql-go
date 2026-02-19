31e96a524f2abcf4275afef466976cf7f2fcb6e0
Abdullah al-araj <alaraj.abdullah02@gmail.com>
2026-02-19 23:00:46 +0300

    add the table and the query

---
+++ sql/queries/feed_follows.sql
@@ -0,0 +1,15 @@
+-- name: CreateFeedFollow :one
+WITH insert_feed_follows AS (
+    INSERT INTO feed_follows (id, user_id, feed_id, created_at, updated_at)
+    VALUES ($1, $2, $3, $4, $5)
+
+    RETURNING *
+)
+
+SELECT     
+    insert_feed_follows.*,
+    feeds.name AS feed_name,
+    users.name AS user_name
+FROM insert_feed_follows
+JOIN users ON users.id = insert_feed_follows.user_id
+JOIN feeds ON feeds.id = insert_feed_follows.feed_id;
